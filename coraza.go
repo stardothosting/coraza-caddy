b69129be4b8ac64cbd7f53184ce7db15d1b2a66c
SDH <admin@stardothosting.com>
2025-04-14 10:54:28 -0400

    test: use AddArgument to set SERVER_NAME

--- coraza.go
+++ coraza.go
@@ -95,8 +95,8 @@ func (m corazaModule) ServeHTTP(w http.ResponseWriter, r *http.Request, next cad
 	id := randomString(16)
 	tx := m.waf.NewTransactionWithID(id)
 
-	// Try using SetVariable directly
-	tx.SetVariable("SERVER_NAME", []string{r.Host})
+	// Try using AddArgument
+	tx.AddArgument("SERVER_NAME", r.Host)
 
 	defer func() {
 		tx.ProcessLogging()
