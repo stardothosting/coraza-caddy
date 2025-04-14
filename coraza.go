1e8c0be5afc4b1d614c2ad23be3a56dfd3232337
SDH <admin@stardothosting.com>
2025-04-14 12:48:16 -0400

    fix: use correct SetRuleEngine method from Coraza v3.0.4

--- coraza.go
+++ coraza.go
@@ -86,7 +86,7 @@ func (m *corazaModule) Provision(ctx caddy.Context) error {
 	}
 
 	// Disable default actions before loading rules
-	config.SetDefaultActionsRuleEngine(false)
+	config.SetRuleEngine(false)
 
 	var err error
 	m.waf, err = coraza.NewWAF(config)
