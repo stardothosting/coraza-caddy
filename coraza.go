eec1af5da4d2ce3fed2690def33e7f8db1353837
SDH <admin@stardothosting.com>
2025-04-14 12:29:44 -0400

    fix: use DisableDefaultRuleActions from Coraza v3.0.4 API

--- coraza.go
+++ coraza.go
@@ -57,6 +57,7 @@ func (corazaModule) CaddyModule() caddy.ModuleInfo {
 func (m *corazaModule) Provision(ctx caddy.Context) error {
 	m.logger = ctx.Logger()
 	config := coraza.NewWAFConfig()
+	config.DisableDefaultRuleActions()
 	if m.Directives != "" {
 		config = config.WithDirectives(m.Directives)
 	}
